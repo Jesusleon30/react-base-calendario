2a5e70ebf72250a6cc61ff42c1284eee71ca450f
Jesus Hugo Retamozo Leon <jesusretamozo@hotmail.it>
2024-08-25 13:22:34 +0200

    aggiunto el react-big-calendar

--- calendario/src/componenti/MainTotal/components/Logo/index.jsx
+++ calendario/src/componenti/MainTotal/components/Logo/index.jsx
@@ -1,13 +1,29 @@
 import "./css/index.css";
+import {Calendar, dayjsLocalizer} from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import dayjs from "dayjs";
 
 export default function Logo() {
+
+  const localizer = dayjsLocalizer(dayjs)
+
+
   return (
     <>
       <section>
         <div>
-          <h1 className="text-white">function logo desde main</h1>
+          <h1 className="testo">function logo desde main</h1>
         </div>
+        <div  className="calendario" >
+          <Calendar  
 
+          localizer={localizer}
+
+          
+          
+          />
+        </div>
+      
       </section>
     </>
   );
